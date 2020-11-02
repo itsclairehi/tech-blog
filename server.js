aa45e6dbe820298c1057a66f1a30044378c779b4
claire p <cpuckett9@gmail.com>
2020-11-02 12:11:45 -0600

    initial files and connect to handlebars

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//handlebars setup
+const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//session setup
+// const session = require('express-session');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+//end session setup
+
+
+//end handlebars setup
+
+// turn on routes
+app.use(routes);
+
+// make styles and html available to server
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at port ${PORT}`));
+});
